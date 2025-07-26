2d3f704f1e469bd075030838bb2024dde056acb3
Ashish Kumar Dash <ashishdash2410@gmail.com>
2025-07-26 18:28:12 +0530

    feat: updated appwrite.hpp

--- include/Appwrite.hpp
+++ include/Appwrite.hpp
@@ -1,6 +1,3 @@
-/// @file Appwrite.hpp
-/// @brief Entry point to access all Appwrite services from the C++ SDK.
-
 #ifndef APPWRITE_HPP
 #define APPWRITE_HPP
 
@@ -19,20 +16,51 @@ class Appwrite {
   public:
     /**
      * @brief Construct a new Appwrite instance.
+     *
      * @param projectId The Appwrite project ID.
      * @param apiKey Optional API key for authenticated requests.
      */
     Appwrite(const std::string &projectId, const std::string &apiKey = "");
 
-    Account &getAccount();     ///< Access Account module
-    Databases &getDatabases(); ///< Access Databases module
-    Storage &getStorage();     ///< Access Storage module
-    Health &getHealth();       ///< Access Health module
-    Messaging &getMessaging(); ///< Access Messaging module
+    /**
+     * @brief Get the Account module to manage user accounts.
+     *
+     * @return Account& Reference to Account object.
+     */
+    Account &getAccount();
+
+    /**
+     * @brief Get the Databases module to perform database operations.
+     *
+     * @return Databases& Reference to Databases object.
+     */
+    Databases &getDatabases();
+
+    /**
+     * @brief Get the Storage module for file and bucket operations.
+     *
+     * @return Storage& Reference to Storage object.
+     */
+    Storage &getStorage();
+
+    /**
+     * @brief Get the Health module to check Appwrite server status.
+     *
+     * @return Health& Reference to Health object.
+     */
+    Health &getHealth();
+
+    /**
+     * @brief Get the Messaging module for managing topics, subscribers, and
+     * emails.
+     *
+     * @return Messaging& Reference to Messaging object.
+     */
+    Messaging &getMessaging();
 
   private:
-    std::string projectId; ///< Project ID
-    std::string apiKey;    ///< API Key
+    std::string projectId;
+    std::string apiKey;
     Account account;
     Databases databases;
     Storage storage;
