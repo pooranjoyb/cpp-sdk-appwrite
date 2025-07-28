ad2de6b4af3705a3f80a62cf610b6f8f69b99890
Pooranjoy <pooranjoyb2016@gmail.com>
2025-07-28 21:22:45 +0530

    refactor: clang-format

--- examples/messaging/messages/createPush.cpp
+++ examples/messaging/messages/createPush.cpp
@@ -3,24 +3,15 @@
 #include <iostream>
 
 int main() {
-    
-    std::string projectId = "68809d54001a8a7c1f72"; 
 
+    std::string projectId = "";
     std::string apiKey = "";
-
     Appwrite appwrite(projectId, apiKey);
-    
     std::string topicId = "67b3048600077f40b8a7";
- 
 
     try {
-
         std::string response = appwrite.getMessaging().createPush(
-            "unique()",
-            "Title ",
-            "Body",
-            topicId
-        );
+            "unique()", "Title ", "Body", topicId);
 
         std::cout << "Push notification created: " << response << std::endl;
     } catch (const AppwriteException &e) {
