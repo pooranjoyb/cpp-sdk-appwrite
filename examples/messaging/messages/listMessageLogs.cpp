f632b8f05f6047f86ff735fdae61b370e1c3507e
bassant ehab <bassantehab60@gmail.com>
2025-07-29 19:33:18 +0000

    removing the apikey value

--- examples/messaging/messages/listMessageLogs.cpp
+++ examples/messaging/messages/listMessageLogs.cpp
@@ -3,12 +3,7 @@
 
 int main() {
     std::string projectId = "68853010003a3f4fc106";
-    std::string apiKey =
-        "standard_"
-        "50c2aa7697484afc2eeda7d9c289df216a2498d8dee455a6fc7c5a0d7ae477600dd349"
-        "6490784034269349bb21d571f16749abf550f4f32cc3c1e8eee5d7314cd1105288cffc"
-        "c4e33e31a7008cbd29f9046a676e848cf51d383eb7259a96394764141206777d2e701b"
-        "d985d345fcc1c1601659e277669155580dba56908569e7"; 
+    std::string apiKey = "";
     std::string messageId = "6888f04d002076dfc07f";
 
     try {
@@ -16,7 +11,7 @@ int main() {
         Queries queries;
         queries.queryLimit(50);
         std::string response =
-        appwrite.getMessaging().listMessageLogs(messageId, queries);
+            appwrite.getMessaging().listMessageLogs(messageId, queries);
         std::cout << "Message logs fetched! \nResponse: " << response
                   << std::endl;
     } catch (const AppwriteException &ex) {
