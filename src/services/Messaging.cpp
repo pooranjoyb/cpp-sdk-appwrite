8dfae7d009b52a5d34e54897020b6a4fc632acbc
bassant ehab <bassantehab60@gmail.com>
2025-08-03 23:33:29 +0000

    list message logs function

--- src/services/Messaging.cpp
+++ src/services/Messaging.cpp
@@ -529,3 +529,23 @@ std::string Messaging::updatePush(const std::string &messageId,
             "\n\nResponse: " + response);
     }
 }
+
+std::string Messaging::listMessageLogs(const std::string &messageId,
+                                       Queries &queries) {
+    if (messageId.empty()) {
+        throw AppwriteException("Missing required parameter: messageId");
+    }
+    std::string url =
+        Config::API_BASE_URL + "/messaging/messages/" + messageId + "/logs";
+    std::vector<std::string> headers = Config::getHeaders(projectId);
+    headers.push_back("X-Appwrite-Key: " + apiKey);
+    std::string response;
+    int statusCode = Utils::getRequest(url, headers, response);
+    if (statusCode == HttpStatus::OK) {
+        return response;
+    } else {
+        throw AppwriteException("Error listing message logs. Status code: " +
+                                std::to_string(statusCode) +
+                                "\nResponse: " + response);
+    }
+}
