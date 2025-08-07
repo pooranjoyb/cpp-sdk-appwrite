e85c5655d437bf7747aee77b65626850c12b81d8
Pooranjoy <pooranjoyb2016@gmail.com>
2025-08-07 13:40:56 +0530

    fix: clang-format

--- examples/messaging/messages/listMessageLogs.cpp
+++ examples/messaging/messages/listMessageLogs.cpp
@@ -1,16 +1,19 @@
 #include "Appwrite.hpp"
 #include <iostream>
 int main() {
-    std::string projectId = "68853010003a3f4fc106";
+    std::string projectId = "66fbb5a100070a3a1d19";
     std::string apiKey = "";
     std::string messageId = "688e98ba00107a10f041";
+
     Appwrite appwrite(projectId, apiKey);
     Queries queries;
+
     queries.queryLimit(50);
     try {
         std::string response =
             appwrite.getMessaging().listMessageLogs(messageId, queries);
-        std::cout << "Message logs fetched! \nResponse: " << response<< std::endl;
+        std::cout << "Message logs fetched! \nResponse: " << response
+                  << std::endl;
     } catch (const AppwriteException &ex) {
         std::cerr << "Exception: " << ex.what() << std::endl;
     }
